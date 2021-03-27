d808f7126aaad358e65a175b90eb08598b7760fe
QRPp <awesome.walrus+github@gmail.com>
2021-03-27 21:32:20 +0000

    + Initial version

---
+++ include/h_log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define FNERR(...) FNLOG(LL_ERROR, ##__VA_ARGS__)
+#define FNERR_AND(and, ...) \
+  do {                           \
+    FNERR(__VA_ARGS__);   \
+    and;                         \
+  } while (0)
+#define FNERR_GT(...) FNERR_GTL(err, ##__VA_ARGS__)
+#define FNERR_GTL(lbl, ...) FNERR_AND(goto lbl, ##__VA_ARGS__)
+#define FNERR_RET(ret, ...) FNERR_AND(return ret, ##__VA_ARGS__)
+#define FNLOG(lvl, fmt, ...) \
+  LOG(lvl, ("%s(): " fmt, __FUNCTION__, ##__VA_ARGS__))
