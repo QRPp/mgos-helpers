1558a383e96d2f91352860a238b3606be2a18d95
QRPp <awesome.walrus+github@gmail.com>
2021-04-23 22:24:04 +0100

    + try_gt() wrapper: call+check+errmsg+goto err
    Also ensure #include of where LOG() lives.

--- include/mgos-helpers/log.h
+++ include/mgos-helpers/log.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <mgos.h>
+
 #define FNERR(...) FNLOG(LL_ERROR, ##__VA_ARGS__)
 #define FNERR_AND(extra, ...) \
   do {                        \
@@ -12,3 +14,9 @@
 #define FNERR_RET(ret, ...) FNERR_AND(return ret, ##__VA_ARGS__)
 #define FNLOG(lvl, fmt, ...) \
   LOG(lvl, ("%s(): " fmt, __FUNCTION__, ##__VA_ARGS__))
+
+#define try_gt(call, ...)                            \
+  ({                                                 \
+    if (!call(__VA_ARGS__)) FNERR_GT("%s() failed", #call); \
+    true;                                            \
+  })
