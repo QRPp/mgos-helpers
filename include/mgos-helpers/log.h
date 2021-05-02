282c4128cec958079d62e7da6b8aae8bbd3ec4e5
QRPp <awesome.walrus+github@gmail.com>
2021-05-02 03:42:00 +0100

    + FNERR_RETF, MUL/S, ON_OFF, TRY_* family

--- include/mgos-helpers/log.h
+++ include/mgos-helpers/log.h
@@ -12,11 +12,21 @@
 #define FNERR_GT(...) FNERR_GTL(err, ##__VA_ARGS__)
 #define FNERR_GTL(lbl, ...) FNERR_AND(goto lbl, ##__VA_ARGS__)
 #define FNERR_RET(ret, ...) FNERR_AND(return ret, ##__VA_ARGS__)
+#define FNERR_RETF(...) FNERR_RET(false, ##__VA_ARGS__)
 #define FNLOG(lvl, fmt, ...) \
   LOG(lvl, ("%s(): " fmt, __FUNCTION__, ##__VA_ARGS__))
 
-#define try_gt(call, ...)                            \
-  ({                                                 \
-    if (!call(__VA_ARGS__)) FNERR_GT("%s() failed", #call); \
-    true;                                            \
+#define MUL(num) MULS(num, "", "s")
+#define MULS(num, s, m) ((num) == 1 ? (s) : (m))
+#define ON_OFF(b) (b ? "ON" : "OFF")
+
+#define TRY_OR(extra, call, ...)                                    \
+  ({                                                                \
+    if (!call(__VA_ARGS__)) FNERR_AND(extra, "%s() failed", #call); \
+    true;                                                           \
   })
+#define TRY_CONT(call, ...) TRY_OR(continue, call, ##__VA_ARGS__)
+#define TRY_GT(call, ...) TRY_GTL(err, call, ##__VA_ARGS__)
+#define TRY_GTL(lbl, call, ...) TRY_OR(goto lbl, call, ##__VA_ARGS__)
+#define TRY_RET(ret, call, ...) TRY_OR(return ret, call, ##__VA_ARGS__)
+#define TRY_RETF(call, ...) TRY_RET(false, call, ##__VA_ARGS__)
