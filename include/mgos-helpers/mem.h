194b9d5429a7ae50c8f45b892c205db93ef7a7ca
QRPp <awesome.walrus+github@gmail.com>
2021-10-12 03:23:12 +0100

    + TRY_CALLOC_OR & TRY_CALLOC_SZ_OR
    A la TRY_MALLOC_OR & TRY_MALLOC_SZ_OR, respectively, but with zeroing.

--- include/mgos-helpers/mem.h
+++ include/mgos-helpers/mem.h
@@ -9,7 +9,9 @@
 #define MALLOC_ERR_ARG(obj) MALLOC_ERR_ARG_SZ(sizeof(*obj))
 #define MALLOC_ERR_ARG_SZ(sz) "malloc", (sz), errno, strerror(errno)
 
-#define TRY_MALLOC_OR(extra, obj) TRY_MALLOC_SZ_OR(extra, sizeof(*obj))
+#define TRY_CALLOC_OR(extra, o) TRY_CALLOC_SZ_OR(extra, sizeof(*o))
+#define TRY_CALLOC_SZ_OR(extra, sz) memset(TRY_MALLOC_SZ_OR(extra, sz), 0, sz)
+#define TRY_MALLOC_OR(extra, o) TRY_MALLOC_SZ_OR(extra, sizeof(*o))
 #define TRY_MALLOC_SZ_OR(extra, sz)                                    \
   ({                                                                   \
     void *mem = malloc(sz);                                            \
