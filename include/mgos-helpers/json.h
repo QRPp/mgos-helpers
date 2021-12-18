a9fdd91b3fcdb504b4a0c3f177f084fc94f55473
QRPp <awesome.walrus+github@gmail.com>
2021-12-18 01:53:19 +0000

    + json.h: JSON_OUT_BUFA()
    Stack-allocates a struct json_out with a buffer of required size.

--- include/mgos-helpers/json.h
+++ include/mgos-helpers/json.h
@@ -7,6 +7,14 @@
 
 #define BOOL_INVAL (sizeof(bool) > sizeof(unsigned char) ? UINT_MAX : UCHAR_MAX)
 
+#define JSON_OUT_BUFA(len)                             \
+  ({                                                   \
+    struct json_out *o = alloca(sizeof(*o));           \
+    size_t l = (len);                                  \
+    *o = (struct json_out) JSON_OUT_BUF(alloca(l), l); \
+    o;                                                 \
+  })
+
 #define TRY_JSON_SCANF_OR(extra, _s, _sz, ...)                           \
   ({                                                                     \
     const char *s = _s;                                                  \
