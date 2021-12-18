81e1b3c28a0e108b48c254c1cb43ae47c4c04569
QRPp <awesome.walrus+github@gmail.com>
2021-12-18 02:23:01 +0000

    * MGOS_OLDER_THAN_S()/_MS(): clang-format

--- include/mgos-helpers/time.h
+++ include/mgos-helpers/time.h
@@ -3,6 +3,8 @@
 #include <mgos_time.h>
 
 #define MGOS_US_SINCE(when) (mgos_uptime_micros() - (when))
-#define MGOS_OLDER_THAN_S(when, s) (MGOS_US_SINCE(when) > ((int64_t) s) * 1000000)
-#define MGOS_OLDER_THAN_MS(when, ms) (MGOS_US_SINCE(when) > ((int64_t) ms) * 1000)
+#define MGOS_OLDER_THAN_S(when, s) \
+  (MGOS_US_SINCE(when) > ((int64_t) s) * 1000000)
+#define MGOS_OLDER_THAN_MS(when, ms) \
+  (MGOS_US_SINCE(when) > ((int64_t) ms) * 1000)
 #define MGOS_OLDER_THAN_US(when, us) (MGOS_US_SINCE(when) > (us))
